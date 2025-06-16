7c9b65ba25ff95abfbaf147748141f8094656fbb
albertegi <albertegi90@gmail.com>
2025-06-16 07:59:22 +0100

    create Text class

---
+++ src/main/java/com/alvirg/e_learning_platform/models/Text.java
@@ -0,0 +1,15 @@
+package com.alvirg.e_learning_platform.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Text {
+
+    private String content;
+}
