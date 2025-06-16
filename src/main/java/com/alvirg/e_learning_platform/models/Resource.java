f6fbab63f2874fa3212fc8d1b6b4051f1e13b1ad
albertegi <albertegi90@gmail.com>
2025-06-16 06:11:28 +0100

    create Resource entity

---
+++ src/main/java/com/alvirg/e_learning_platform/models/Resource.java
@@ -0,0 +1,29 @@
+package com.alvirg.e_learning_platform.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+public class Resource {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private int size;
+
+    private String url;
+
+
+}
