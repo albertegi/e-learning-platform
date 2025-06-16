53000d55e92e6effbe2ef509fb0e035509afa7d4
albertegi <albertegi90@gmail.com>
2025-06-16 02:45:11 +0100

    update on Author entity

--- src/main/java/com/alvirg/e_learning_platform/models/Author.java
+++ src/main/java/com/alvirg/e_learning_platform/models/Author.java
@@ -1,23 +1,74 @@
 package com.alvirg.e_learning_platform.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
+//@Table(name = "AUTHOR_TBL")
 public class Author {
 
     @Id
     @GeneratedValue
+            /*(
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )*/
+
+    //@GeneratedValue(
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "author_sequence"
+//    )
+//    // manually create "author_sequence"
+//    @SequenceGenerator(
+//            name = "author_sequence",
+//            sequenceName = "author_sequence",
+//            allocationSize = 1
+//    )
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
+    @Column(
+            name = "l_name",
+            length = 35
+    )
+    private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private int age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 }
+
+
+
