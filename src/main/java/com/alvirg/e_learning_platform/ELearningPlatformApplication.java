404f935f487f3843da63854f1c758ad868985316
albertegi <albertegi90@gmail.com>
2025-06-16 05:47:58 +0100

    add CommandLineRunner Bean to safely save the author details in the database

--- src/main/java/com/alvirg/e_learning_platform/ELearningPlatformApplication.java
+++ src/main/java/com/alvirg/e_learning_platform/ELearningPlatformApplication.java
@@ -1,13 +1,32 @@
 package com.alvirg.e_learning_platform;
 
+import com.alvirg.e_learning_platform.models.Author;
+import com.alvirg.e_learning_platform.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ELearningPlatformApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ELearningPlatformApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthorRepository authorRepository){
+		return args -> {
+			// create an object of the Author Entity and use the @Builder
+			var author = Author.builder()
+					.firstName("Albert")
+					.lastName("Egi")
+					.age(37)
+					.email("albertegi@gmail.com")
+					.build();
+			authorRepository.save(author);
+		};
 	}
 
 }
--- src/main/java/com/alvirg/e_learning_platform/models/Author.java
+++ src/main/java/com/alvirg/e_learning_platform/models/Author.java
@@ -1,41 +1,22 @@
 package com.alvirg.e_learning_platform.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 @Entity
-//@Table(name = "AUTHOR_TBL")
 public class Author {
 
     @Id
     @GeneratedValue
-            /*(
-            strategy = GenerationType.TABLE,
-            generator = "author_id_gen"
-    )
-    @TableGenerator(
-            name = "author_id_gen",
-            table = "id_generator",
-            pkColumnName = "id_name",
-            valueColumnName = "id_value",
-            allocationSize = 1
-    )*/
-
-    //@GeneratedValue(
-//            strategy = GenerationType.SEQUENCE,
-//            generator = "author_sequence"
-//    )
-//    // manually create "author_sequence"
-//    @SequenceGenerator(
-//            name = "author_sequence",
-//            sequenceName = "author_sequence",
-//            allocationSize = 1
-//    )
     private Integer id;
 
     @Column(
@@ -58,16 +39,6 @@ public class Author {
 
     private int age;
 
-    @Column(
-            updatable = false,
-            nullable = false
-    )
-    private LocalDateTime createdAt;
-
-    @Column(
-            insertable = false
-    )
-    private LocalDateTime lastModified;
 }
 
 
