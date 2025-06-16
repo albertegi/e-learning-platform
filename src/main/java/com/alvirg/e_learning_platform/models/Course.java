f9f5a457de36da13e44c789a084fe860bde792b9
albertegi <albertegi90@gmail.com>
2025-06-16 06:02:04 +0100

    create the Course entity

---
+++ src/main/java/com/alvirg/e_learning_platform/models/Course.java
@@ -0,0 +1,25 @@
+package com.alvirg.e_learning_platform.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String description;
+}
