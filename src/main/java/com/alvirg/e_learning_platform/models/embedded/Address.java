0cc69ab021c1027cd0ff10abb4175535cafed724
albertegi <albertegi90@gmail.com>
2025-06-17 19:23:02 +0100

    add Address class with three properties and update Order with @Embedded annotation

--- src/main/java/com/alvirg/e_learning_platform/models/embedded/Address.java
+++ src/main/java/com/alvirg/e_learning_platform/models/embedded/Address.java
@@ -1,4 +1,21 @@
 package com.alvirg.e_learning_platform.models.embedded;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
 public class Address {
+
+    private String streetName;
+
+    private String houseNumber;
+
+    private String zipCode;
 }
--- src/main/java/com/alvirg/e_learning_platform/models/embedded/Order.java
+++ src/main/java/com/alvirg/e_learning_platform/models/embedded/Order.java
@@ -1,5 +1,6 @@
 package com.alvirg.e_learning_platform.models.embedded;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -17,6 +18,10 @@ public class Order {
     // create an instance of OrderId
     @EmbeddedId
     private OrderId id;
+
+    // to tell that this Address is an embeddable entity within my Order entity
+    @Embedded
+    private Address address;
 
     private String orderInfo;
 
