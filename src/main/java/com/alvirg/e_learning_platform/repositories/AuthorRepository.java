ea877ecb213b31ce6ba8533b49f1c5f3cea7e4b5
albertegi <albertegi90@gmail.com>
2025-06-17 19:57:09 +0100

    update AuthorRepository with derived query methods

--- src/main/java/com/alvirg/e_learning_platform/repositories/AuthorRepository.java
+++ src/main/java/com/alvirg/e_learning_platform/repositories/AuthorRepository.java
@@ -3,5 +3,22 @@ package com.alvirg.e_learning_platform.repositories;
 import com.alvirg.e_learning_platform.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    // This query is the equivalent of --> select * from author where first_name = ''
+    List<Author> findAllByFirstName(String fn);
+
+    // This query is the equivalent of --> select * from author where first_name = 'al'
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    // This query is the equivalent of --> select * from author where first_name like '%al%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    // This query is the equivalent of --> select * from author where first_name like 'al%'
+    List<Author> findAllByFirstNameStartingWithIgnoreCase(String fn);
+
+    // This query is the equivalent of --> select * from author where first_name like '%al'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
 }
